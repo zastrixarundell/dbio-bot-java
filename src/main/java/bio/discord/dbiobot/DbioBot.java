903338a4baf277f3c823d07250b52bca50e2c609
zastrixarundell <zastrixarundell@gmail.com>
2020-05-02 12:42:54 +0200

    Added README

--- src/main/java/bio/discord/dbiobot/DbioBot.java
+++ src/main/java/bio/discord/dbiobot/DbioBot.java
@@ -6,36 +6,30 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
-import org.javacord.api.listener.message.MessageCreateListener;
 
-public class DbioBot implements MessageCreateListener
+public class DbioBot
 {
-
     public static void main(String[] args)
     {
         String token = System.getenv("DISCORD_BOT_TOKEN");
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
-        api.addMessageCreateListener(new DbioBot());
+        api.addMessageCreateListener(messageCreateEvent -> {
+            if(!messageCreateEvent.getMessageAuthor().isRegularUser())
+                return;
+
+            String content = messageCreateEvent.getMessageContent();
+
+            if(!content.startsWith(".info"))
+                return;
+
+            Dbio.getUserDetails(messageCreateEvent.getMessageAuthor().getIdAsString()).thenAcceptAsync(user ->
+                    user.ifPresent(theUser -> sendUserInfo(theUser, messageCreateEvent)));
+        });
     }
 
-    @Override
-    public void onMessageCreate(MessageCreateEvent messageCreateEvent)
-    {
-        if(!messageCreateEvent.getMessageAuthor().isRegularUser())
-            return;
-
-        String content = messageCreateEvent.getMessageContent();
-
-        if(!content.startsWith(".info"))
-            return;
-
-        Dbio.getUserDetails(messageCreateEvent.getMessageAuthor().getIdAsString()).thenAcceptAsync(user ->
-                user.ifPresent(theUser -> sendUserInfo(theUser, messageCreateEvent)));
-    }
-
-    private void sendUserInfo(User user, MessageCreateEvent event)
+    private static void sendUserInfo(User user, MessageCreateEvent event)
     {
         EmbedBuilder builder = new EmbedBuilder()
                 .setTitle(user.getDiscordInformation().getFullUserUsername())
